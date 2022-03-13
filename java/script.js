326348672992ae448bb85621738fb596ef75ac87
JohnKOnyango <johnonyango399@gmail.com>
2022-03-13 18:29:18 +0300

    added toggle function on development icon

--- java/script.js
+++ java/script.js
@@ -7,20 +7,20 @@
       $("#design").slideUp();
       $("#d-image").slideDown("2000");
     });
-    $("#d-image").click(function(){
-        $("#d-image").slideDown("2000");
-        $("#design").show(2000);
+    $("#d-icon").click(function(){
+        $("#d-icon").slideDown("2000");
+        $("#develop").show(2000);
       });
-      $("#design").click(function(){
-        $("#design").slideUp();
-        $("#d-image").slideDown("2000");
+      $("#develop").click(function(){
+        $("#develop").slideUp();
+        $("#d-icon").slideDown("2000");
       });
-      $("#d-image").click(function(){
-        $("#d-image").slideDown("2000");
-        $("#design").show(2000);
-      });
-      $("#design").click(function(){
-        $("#design").slideUp();
-        $("#d-image").slideDown("2000");
-      });
+       $("#d-image").click(function(){
+      $("#d-image").slideDown("2000");
+      $("#design").show(2000);
+    });
+    $("#design").click(function(){
+      $("#design").slideUp();
+      $("#d-image").slideDown("2000");
+    });
   
