78a1bd1d2977fa1bb01c683fd5507aa28564d975
JohnKOnyango <johnonyango399@gmail.com>
2022-03-13 18:32:50 +0300

    added toggle on product management icon

--- java/script.js
+++ java/script.js
@@ -15,12 +15,12 @@
         $("#develop").slideUp();
         $("#d-icon").slideDown("2000");
       });
-       $("#d-image").click(function(){
-      $("#d-image").slideDown("2000");
-      $("#design").show(2000);
+       $("#pro_icon").click(function(){
+      $("#pro_icon").slideDown("2000");
+      $("#prod").show(2000);
     });
-    $("#design").click(function(){
-      $("#design").slideUp();
-      $("#d-image").slideDown("2000");
+    $("#prod").click(function(){
+      $("#prod").slideUp();
+      $("#pro_icon").slideDown("2000");
     });
   
