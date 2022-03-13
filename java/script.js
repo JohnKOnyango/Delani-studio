ea428901d0f485517462450657abd84316cb54b6
JohnKOnyango <johnonyango399@gmail.com>
2022-03-13 18:25:35 +0300

    added toggle function on the design icon

---
+++ java/script.js
@@ -0,0 +1,26 @@
+
+    $("#d-image").click(function(){
+      $("#d-image").slideDown("2000");
+      $("#design").show(2000);
+    });
+    $("#design").click(function(){
+      $("#design").slideUp();
+      $("#d-image").slideDown("2000");
+    });
+    $("#d-image").click(function(){
+        $("#d-image").slideDown("2000");
+        $("#design").show(2000);
+      });
+      $("#design").click(function(){
+        $("#design").slideUp();
+        $("#d-image").slideDown("2000");
+      });
+      $("#d-image").click(function(){
+        $("#d-image").slideDown("2000");
+        $("#design").show(2000);
+      });
+      $("#design").click(function(){
+        $("#design").slideUp();
+        $("#d-image").slideDown("2000");
+      });
+  
